a8fbe783b9104dc87bd682a5dd9e545c27aecc06
znnznn <znnznn@ukr.net>
2020-11-19 23:49:19 +0200

    зробив вьюшку контактів додав функцію яка вертає мені титулку і зробив вьшку logout

--- my_django_project/main/views.py
+++ my_django_project/main/views.py
@@ -1,5 +1,5 @@
 import json
-
+import  datetime
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout, login, authenticate, get_user_model
@@ -8,38 +8,57 @@ from django.urls import reverse
 from django.http import HttpResponseRedirect
 
 from .tradier_api import symbol_stocks
+from .models import Message
 # Create your views here.
 
 
 amount_stock = 10
 
 
+def check_user(request):
+    if request.user.is_authenticated:
+        title = request.user.username
+    else:
+        title = 'Біржовий аналітик'
+    return title
+
+
 def index(request):
-    title = {'title': 'col'}
+    title = check_user(request)
     return render(request, 'main/index.html', context=title)
 
 
 def contacts(request):
-    return render(request, 'main/contacts.html')
+    title = check_user(request)
+    if request.POST:
+        data_message = dict(request.POST)
+        message_user = Message(user_name=data_message['username'][0], user_email=data_message['email'][0],
+                               message=data_message['message'][0])
+        message_user.save()
+        message = f'{data_message["username"][0]} Ваше повідомлення відправлено.'
+        return render(request, 'main/contacts.html', context={"message": message,
+                                                              'title': title})
+    return render(request, 'main/contacts.html', context={'title': title})
 
 
 def login_page(request):
+    title = check_user(request)
+    if request.user.is_authenticated:
+        return HttpResponseRedirect(redirect_to='user')
     if request.POST:
         data_user = dict(request.POST)
         user = authenticate(username=data_user['email'][0], password=data_user['password'][0])
         if user:
             login(request, user)
-
-            print(User.objects.get(username=user))
-            message = f'{user.username} вітаємо!'
             return HttpResponseRedirect(redirect_to='user')
         else:
             message = f'Користувача з такими даними не існує.'
         return render(request, 'main/login.html', context={'user': user, 'message': message})
-    return render(request, 'main/login.html')
+    return render(request, 'main/login.html', context={'title': title})
 
 
 def new_user(request):
+    title = check_user(request)
     if request.POST:
         data_user = dict(request.POST)
         if data_user['email'][0] != data_user['username'][0]:
@@ -57,8 +76,8 @@ def new_user(request):
             message = "Сталась помлка з\'днання з базою даних"
         else:
             message = f'{data_user["firstName"][0]} Ви успішно зареєструвались.'
-        return render(request, 'main/login.html', context={'message': message, 'user': user})
-    return render(request, 'main/new_user.html')
+        return render(request, 'main/login.html', context={'message': message, 'title': title})
+    return render(request, 'main/new_user.html', context={'title': title})
 
 
 @login_required(login_url='/login')
@@ -69,7 +88,6 @@ def profile_user(request):
 @login_required(login_url='/login')
 def user_page(request):
     user = request.user
-    print(user)
     with open('main/stocks_all.json', 'r') as file_stocks:
         list_stocks = json.load(file_stocks)
         my_stocks = list_stocks['securities']['security']
@@ -84,8 +102,6 @@ def user_page(request):
             my_stocks[i]['positive_change'] = False
         my_stocks[i]['quote'] = data_symbol['quotes']['quote']
         i += 1
-    print(my_stocks[0])
-    print(my_stocks[0].keys())
     return render(request, 'main/user.html', context={'user': user, 'title': user.username, 'stocks': my_stocks})
 
 
@@ -123,5 +139,8 @@ def del_user(request):
 
 @login_required(login_url='/login')
 def logout_user(request):
+    title = check_user(request)
+    message = f'До зустрічі {title}'
     logout(request)
-    return render(request, 'main/index.html')
+    title = check_user(request)
+    return render(request, 'main/index.html', context={'title': title, 'message': message})
