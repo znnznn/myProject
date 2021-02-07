c99999009a6529795e5fd8540d67cb3971d7cc36
znnznn <znnznn@ukr.net>
2021-02-07 21:43:54 +0200

    підчистив авто формування значення

--- my_django_project/main/models.py
+++ my_django_project/main/models.py
@@ -23,7 +23,29 @@ class TradeStock(models.Model):
     prevclose = models.DecimalField(max_digits=50, decimal_places=5, verbose_name='Попередня ціна закриття')
     week_52_high = models.DecimalField(max_digits=50, decimal_places=5, verbose_name='Найвижча ціна за рік')
     week_52_low = models.DecimalField(max_digits=50, decimal_places=5, verbose_name='Найнижча ціна за рік')
-    trade_date = models.DateTimeField(verbose_name='Дата купівлі')
+    trade_date = models.DateTimeField(auto_now=True, verbose_name='Дата купівлі')
+
+    def data_stock(self, stock: dict):
+        self.symbol = stock['symbol']
+        self.description = stock['description']
+        self.exch = stock['exch']
+        self.stock_type = stock['type']
+        self.open_price = stock['open']
+        self.high_price = stock['high']
+        self.low_price = stock['low']
+        self.bid_price = stock['bid']
+        self.ask_price = stock['ask']
+        self.change_percentage = stock['change_percentage']
+        self.prevclose = stock['prevclose']
+        self.week_52_high = stock['week_52_high']
+        self.week_52_low = stock['week_52_low']
+        try:
+            self.save()
+            return True
+        except:
+            return False
+
+
 
 
 class Message(models.Model):
@@ -32,4 +54,4 @@ class Message(models.Model):
     user_name = models.CharField(max_length=50, verbose_name='Name')
     user_email = models.CharField(max_length=50, verbose_name='Email')
     message = models.TextField(max_length=500, verbose_name='message')
-    msg_date = models.DateTimeField(verbose_name='message_date')
+    msg_date = models.DateTimeField(auto_now=True, verbose_name='message_date')
